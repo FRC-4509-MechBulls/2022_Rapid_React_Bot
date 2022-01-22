b4a926b8ab01b87b705bd62bd243a5b1c0de5c1d
Morgan Nash <morganmaenash@gmail.com>
2022-01-22 14:46:32 -0500

    indexersub, intake sols
    Co-authored-by: EnderNate <EnderNate@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -15,18 +14,24 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    //Falcons
+    //DriveTrain
     public static final int FRONT_LEFT_TALON = 1;
     public static final int FRONT_RIGHT_TALON = 2;
     public static final int BACK_LEFT_TALON = 3;
     public static final int BACK_RIGHT_TALON = 4;
     
-    public static final int SHOOTER_TALON_1 = 5;
-    public static final int SHOOTER_TALON_2 = 6;
-    public static final int SHOOTER_TALON_3 = 7;
-    public static final int SHOOTER_TALON_4 = 8;
-
+    //Shooter
+    public static final int SHOOTER_FALCON_1 = 5;
+    public static final int SHOOTER_FALCON_2 = 6;
+    public static final int SHOOTER_TALON = 7;
+    
     public static final int HOOD_TALON = 0;
+
+    //Indexer
+    public static final int INDEX_BELT_1_TALON = 0; //figure out later
+    public static final int INDEX_BELT_2_TALON = 0; //figure out later
+    public static final int INDEX_KICK_1_TALON = 0; //figure out later
+    public static final int INDEX_KICK_2_TALON = 0; //figure out later
 
     //Speeds
     public static final double DRIVETRAIN_SPEED = 0;
@@ -35,15 +40,21 @@ public final class Constants {
     public static final double SHOOTER_SPEED_TOP = 1.0;
 
     //Controller Inputs
+    public static final int DRIVER_CONTROLLER = 0;
+    public static final int SHOOTER_CONTROLLER = 1;
     public static final int RIGHT_TRIGGER = 3;
     public static final int LEFT_TRIGGER = 2;
     public static final int XBOX_LEFT_X_AXIS = 0;
     
+    //Intake
     public static final int INTAKE = 11;
     public static final double INTAKE_SPEED = 0.5;
+    public static final int INTAKE_2_REVERSE = 0;
+    public static final int INTAKE_2_FORWARD = 1;
+    public static final int INTAKE_1_REVERSE = 2;
+    public static final int INTAKE_1_FORWARD = 3;
    
-    public static final int DRIVER_CONTROLLER = 0;
-    public static final int SHOOTER_CONTROLLER = 1;
+   
 
     //Solenoid Channels
     public static final int SHIFTER_1_FORWARD_CHANNEL = 0;
--- src/main/java/frc/robot/subsystems/IntakeSub.java
+++ src/main/java/frc/robot/subsystems/IntakeSub.java
@@ -6,27 +6,52 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class IntakeSub extends SubsystemBase {
+  DoubleSolenoid intakeSolenoid1;
+  DoubleSolenoid intakeSolenoid2;
   WPI_TalonFX intake;
   /** Creates a new IntakeSub. */
   public IntakeSub() {
-   intake = new WPI_TalonFX(Constants.INTAKE);
+    intake = new WPI_TalonFX(Constants.INTAKE);
+    intakeSolenoid1 = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.INTAKE_1_FORWARD, Constants.INTAKE_1_REVERSE);
+    intakeSolenoid2 = new DoubleSolenoid(PneumaticsModuleType.REVPH, Constants.INTAKE_2_FORWARD, Constants.INTAKE_2_REVERSE);
   }
 
+  //temporary methods for actuating intake, might change later depending on logic
+  public void deployIntake1() {
+    intakeSolenoid1.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void deployIntake2() {
+    intakeSolenoid2.set(DoubleSolenoid.Value.kForward);
+  }
+
+
+  public void retractIntake1() {
+    intakeSolenoid1.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  public void retractIntake2() {
+    intakeSolenoid2.set(DoubleSolenoid.Value.kReverse);
+  }
   
-  @Override
+  public void deployIntakeTalon(double speed) {
+    intake.set(speed);
+  }
+
+  public void retractIntakeTalon() {
+    intake.set(0);
+  } 
+
+@Override
   public void periodic() {
     // This method will be called once per scheduler run
- }
-public void deployIntake(double speed) {
-intake.set(speed);
-}
-public void retractIntake() {
-intake.set(0);
-}
+  }
 
 
 }
